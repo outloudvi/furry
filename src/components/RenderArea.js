77f50568ae01d88ca37a62675c6975c17329bbb6
Outvi V <i+oss.furry@outv.im>
2023-02-25 21:45:14 +0800

    feat: click-to-scroll

--- src/components/RenderArea.js
+++ src/components/RenderArea.js
@@ -1,10 +1,23 @@
-import { RenderZone, SectionRender } from '../styled'
+import { useEffect } from 'react'
+import { SectionRender } from '../styled'
 
 const RenderArea = ({ result, hilightArea }) => {
+  useEffect(() => {
+    if (hilightArea === null) return
+    const targetItem = document.querySelector(`#line--${hilightArea[0]}`)
+    if (!targetItem) return
+
+    targetItem.scrollIntoView({
+      behavior: 'smooth',
+      block: 'nearest',
+    })
+  }, [hilightArea])
+
   return (
     <SectionRender>
       {result.map((item, key) => (
         <p
+          id={`line--${key + 1}`}
           key={key}
           dangerouslySetInnerHTML={{
             __html: item,
